171c024aadf156334c48ce493384113a0fc2fcb9
Nika <92479422+nikyn4ik@users.noreply.github.com>
2023-03-11 01:19:14 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -6,3 +6,6 @@ using namespace std
   class Tree {
     private:
     vector <vector<pair<Person, pair<Person, Person>>>> tree;
+    public:
+    Tree();
+    void add_person(Person p, Person father, Person mother); // Add a person with two parents
