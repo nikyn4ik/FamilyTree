5045da453863212dd63137a224c3d20624b33a87
Nika <92479422+nikyn4ik@users.noreply.github.com>
2023-04-15 23:08:54 +0300

    add comments

--- FamilyTree/Main.cpp
+++ FamilyTree/Main.cpp
@@ -112,7 +112,7 @@ int main() {
 	t.add_person(Person("Grandfather on mother's side", "30.01.1935", "26.12.1993"));//G0
 	t.add_person(Person("Grandmother on father's side", "23.10.1944"));//G0
 	t.add_person(Person("Grandfather on father's side", "22.11.1945", "10.09.1996"));//G0
-
+    //удаление последних двух строк
 	t.add_person(Person("Mother", "28.11.1967"), Person("Grandmother on mother's side", "20.11.1934"), Person("Grandfather on mother's side", "30.01.1935", "26.12.1993"));//G1
 	t.add_person(Person("Father", "01.12.1968", "02.08.2019"), Person("Grandmother on father's side", "23.10.1944"), Person("Grandfather on father's side", "22.11.1945", "10.09.1996"));//G1
 
