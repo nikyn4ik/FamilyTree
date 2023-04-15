3921d2c82be2f234e16bb500f68371cde2275cea
Nika <92479422+nikyn4ik@users.noreply.github.com>
2023-04-15 23:08:30 +0300

    Fix family tree

--- FamilyTree/Main.cpp
+++ FamilyTree/Main.cpp
@@ -116,6 +116,10 @@ int main() {
 	t.add_person(Person("Mother", "28.11.1967"), Person("Grandmother on mother's side", "20.11.1934"), Person("Grandfather on mother's side", "30.01.1935", "26.12.1993"));//G1
 	t.add_person(Person("Father", "01.12.1968", "02.08.2019"), Person("Grandmother on father's side", "23.10.1944"), Person("Grandfather on father's side", "22.11.1945", "10.09.1996"));//G1
 
+	t.add_person(Person("it's me", "01.03.2002"), Person("Mother", "28.11.1967"), Person("Father", "01.12.1968", "02.08.2019"));//G2
+
+	t.add_person(Person("My descendant from the future", "30.06.2934"), Person("it's me", "01.03.2002"));//G3
+
     int choice;
     while (true) {
         std::cout << "Menu:\n";
